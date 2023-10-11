3dfead6b5ac4037fb167eea7f14fbe9ff52ff365
david.munoz.7e7 <david.munoz.7e7@itb.cat>
2023-10-11 12:44:11 +0200

    Ex2

--- Ex2.py
+++ Ex2.py
@@ -6,8 +6,17 @@ Didac Lloret
 AirVolumeCalculator
 ASIXc 1B
 """
-
+#Try per si el usuari no posa numeros
 try:
-    float(input("el ample del aula en (m) és:"))
-    float(input("l'alçada del aula en (m) és:"))
-    float(input("la longitud del aula en (m)"))
+    #Demanar les dades de l'habitació
+    ample = float(input("el ample del aula en (m) és:"))
+    longitud = float(input("la longitud del aula en (m)"))
+    alçada = float(input("l'alçada del aula en (m) és:"))
+
+    #Calcular el volumi mostrar en pantalla
+    solucio = ample * alçada * longitud
+    print ("El volum de l'aula és", solucio,"m³")
+#except ValueError per si posa lletres
+
+except ValueError:
+    print("nomes números cara anchoa")
